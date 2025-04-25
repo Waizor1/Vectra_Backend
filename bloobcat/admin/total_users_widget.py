85c9c9f02b5746a6bd4007eea4351d3053a15be5
FreeThunder11 <gurov.yuron2@gmail.com>
2025-04-25 20:43:05 +0300

    Enhance user dashboard widgets with date filtering and period selection
    - Added date picker widget type and properties for active, inactive, and total users dashboard widgets.
    - Implemented dynamic date range selection for querying user data based on registration dates.
    - Updated SQL queries to filter results by specified date ranges and periods (day, week, month, year).
    - Improved return values to include dynamically set min and max date fields for better data handling.

--- bloobcat/admin/total_users_widget.py
+++ bloobcat/admin/total_users_widget.py
@@ -16,6 +16,9 @@ class TotalUsersDashboardWidgetAdmin(DashboardWidgetAdmin):
 
     x_field = "date"
     y_field = "count"
+    x_field_filter_widget_type = WidgetType.DatePicker
+    x_field_filter_widget_props: dict[str, str] = {"picker": "day"}  # noqa: RUF012
+    x_field_periods = ["day", "week", "month", "year"]  # noqa: RUF012
 
     async def get_data(
         self,
@@ -24,26 +27,40 @@ class TotalUsersDashboardWidgetAdmin(DashboardWidgetAdmin):
         period_x_field: str | None = None,
     ) -> dict:
         conn = Tortoise.get_connection("default")
-        
-        sql_query = """
-            WITH user_counts AS (
-                SELECT 
-                    registration_date::date AS dt,
-                    COUNT(*) OVER (ORDER BY registration_date::date) AS count
+
+        if not min_x_field:
+            min_x_field_date = datetime.datetime.now(
+                tz=datetime.UTC
+            ) - datetime.timedelta(days=360)
+        else:
+            min_x_field_date = datetime.datetime.fromisoformat(min_x_field)
+        if not max_x_field:
+            max_x_field_date = datetime.datetime.now(
+                tz=datetime.UTC
+            ) + datetime.timedelta(days=1)
+        else:
+            max_x_field_date = datetime.datetime.fromisoformat(max_x_field)
+
+        if not period_x_field or period_x_field not in (
+            self.x_field_periods or []
+        ):
+            period_x_field = "day"
+
+        results = await conn.execute_query_dict(
+            """
+                SELECT
+                    to_char(date_trunc($1, registration_date)::date, 'DD/MM/YYYY') AS date,
+                    COUNT(*) AS count
                 FROM users
-                ORDER BY dt
-            )
-            SELECT 
-                to_char(dt, 'DD/MM/YYYY') AS date,
-                count
-            FROM user_counts
-        """
-        
-        results = await conn.execute_query_dict(sql_query, [])
-        
+                WHERE registration_date >= $2 AND registration_date <= $3
+                GROUP BY date_trunc($1, registration_date)
+                ORDER BY date_trunc($1, registration_date)
+            """,
+            [period_x_field, min_x_field_date, max_x_field_date],
+        )
         return {
             "results": results,
-            "min_x_field": "2023-01-01",
-            "max_x_field": "2025-12-31",
-            "period_x_field": "day",
+            "min_x_field": min_x_field_date.isoformat(),
+            "max_x_field": max_x_field_date.isoformat(),
+            "period_x_field": period_x_field,
         } 
