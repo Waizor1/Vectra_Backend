1baa222bb79dd8e560f45372409d36c3c146d4cf
FreeThunder11 <gurov.yuron2@gmail.com>
2025-04-25 13:16:55 +0300

    Enhance alerts worker to exclude trial users from subscription expiration notifications
    - Updated the alerts worker logic to ensure that trial users are not notified about subscription expirations.
    - Improved the reply markup for the subscription renewal message by awaiting the inline button generation.

--- bloobcat/bot/alerts.py
+++ bloobcat/bot/alerts.py
@@ -2,14 +2,13 @@ from bloobcat.bot import bot
 from bloobcat.bot.keyboard import webapp_inline_button
 from bloobcat.db.users import Users
 
-
 async def alerts_worker():
     users = await Users.all()
     for user in users:
         expires_days = user.expires()
-        if expires_days is not None and 1 <= expires_days <= 3:
+        if not user.is_trial and expires_days is not None and 1 <= expires_days <= 3:
             await bot.send_message(
                 user.id,
                 f"❗ Ваш тариф истечет через {expires_days} {'день' if expires_days == 1 else 'дня' if expires_days == 2 else 'дня'}. Пожалуйста, продлите подписку в личном кабинете.",
-                reply_markup=webapp_inline_button("Продлить подписку", "pay"),
+                reply_markup=await webapp_inline_button("Продлить подписку", "pay"),
             )
