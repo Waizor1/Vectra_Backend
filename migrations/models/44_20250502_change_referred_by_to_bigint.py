9ff6e3b6f368f7011c010e42d93c34edb53d3836
FreeThunder11 <gurov.yuron2@gmail.com>
2025-04-28 13:33:16 +0300

    Update SQL migration to use consistent quotation marks for `referred_by` column type changes in Users table

--- migrations/models/44_20250502_change_referred_by_to_bigint.py
+++ migrations/models/44_20250502_change_referred_by_to_bigint.py
@@ -2,10 +2,10 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE \"users\" ALTER COLUMN \"referred_by\" TYPE BIGINT USING \"referred_by\"::BIGINT;
+        ALTER TABLE "users" ALTER COLUMN "referred_by" TYPE BIGINT USING "referred_by"::BIGINT;
     """
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE \"users\" ALTER COLUMN \"referred_by\" TYPE INTEGER USING \"referred_by\"::INTEGER;
+        ALTER TABLE "users" ALTER COLUMN "referred_by" TYPE INTEGER USING "referred_by"::INTEGER;
     """ 
