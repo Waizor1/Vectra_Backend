e9a15cfc0f65bf5693705f8c24f5c6752969c535
FreeThunder11 <gurov.yuron2@gmail.com>
2025-04-29 21:10:39 +0300

    Enhance admin statistics and online user reporting
    - Improved the formatting of the admin statistics message to include registration and payment percentages for better insights.
    - Updated the online user reporting message to use HTML formatting for a more visually appealing output.
    - Removed unnecessary blank lines and streamlined the code for clarity.

--- bloobcat/bot/routes/admin/stat.py
+++ bloobcat/bot/routes/admin/stat.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime, timedelta
 
 from aiogram.dispatcher.router import Router
@@ -23,19 +22,27 @@ async def admin_stat(message: Message, command: CommandObject):
         if expires_days is not None and expires_days > 3:
             payed += 1
 
-    await message.answer(f"""Статистика {utm}
-Всего зашли: {amount}
-Активировали: {registered}
-Оплатили: {payed}""")
+    now = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    percent_registered = amount and registered / amount * 100 or 0
+    percent_payed = registered and payed / registered * 100 or 0
+
+    stats_message = (
+        f"📊 <b>Статистика по UTM:</b> <code>{utm}</code>\n"
+        f"<i>отчет на {now}</i>\n\n"
+        f"👥 Всего зашли: <b>{amount}</b>\n"
+        f"✅ Активировали: <b>{registered}</b> (<i>{percent_registered:.1f}%</i>)\n"
+        f"💰 Оплатили: <b>{payed}</b> (<i>{percent_payed:.1f}%</i>)"
+    )
+    await message.answer(stats_message, parse_mode="HTML")
 
 
 @router.message(Command("online"), IsAdmin())
 async def online_(message: Message):
-    m = await message.answer("Подождите...")
+    m = await message.answer("⏳ <i>Подождите...</i>", parse_mode="HTML")
 
     active_users = await Users.filter(
         connected_at__gte=datetime.now(UTC) - timedelta(minutes=3)
     )
     i = len(active_users)
 
-    await m.edit_text(f"Онлайн: {i}")
+    await m.edit_text(f"👥 <b>Сейчас онлайн:</b> <code>{i}</code>", parse_mode="HTML")
